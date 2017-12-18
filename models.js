eda3abf825892654a238721b9383aba9a47dbba8
KhishorekumarBS <khishkrish@gmail.com>
2017-12-18 23:52:17 +0530

    updated signupmodel
    bcrypt password comparison is added

--- models.js
+++ models.js
@@ -5,5 +5,38 @@ var signupschema=new schema({
 	email:String,
     pass:String,
 });
-var user=mongoose.model('user',signupschema)
+
+var bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
+
+
+signupschema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('pass')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the pass using our new salt
+        bcrypt.hash(user.pass, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext pass with the hashed one
+            user.pass = hash;
+            next();
+        });
+    });
+});
+
+signupschema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.pass, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+var user=mongoose.model('user',signupschema);
 module.exports=user;
